587c7dd99d9fd3a3116da4ace69a8ccee4ee4928
Christiane Hertwig <christiane.hertwig@googlemail.com>
2022-07-30 13:32:26 +0200

    sicherheitskopie

--- src/components/AddItem.jsx
+++ src/components/AddItem.jsx
@@ -2,6 +2,7 @@ import StyledSearchBar from "./StyledSearchBar";
 import React from "react";
 import { useState } from "react";
 import useStore from "../useStore.js";
+/* import { search } from 'fast-fuzzy'; */
 
 export default function AddItem() {
   const [inputValue, setInputValue] = useState("");
@@ -21,10 +22,24 @@ export default function AddItem() {
           required
           value={inputValue}
           onChange={(event) => {
+            //hier könnte fuzzy passieren
             setInputValue(event.target.value);
           }}
+          /*   onChange={(event) => {
+            setInputValue(search(event.target.value)) 
+          }} */
         ></input>
       </label>
     </StyledSearchBar>
   );
 }
+
+/* const {search} = require("fast-fuzzy");
+
+//pass in a keySelector to search for objects
+search(
+    "abc",   INPUTVALUE
+    [{name: "def"}, {name: "bcd"}, {name: "cde"}, {name: "abc"}],
+    {keySelector: (obj) => obj.name},
+);
+//returns [{name: "abc"}, {name: "bcd"}] */
