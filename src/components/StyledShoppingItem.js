10285bc0cf52e0aff88df49ac056ffb89c0a1ba0
Christiane Hertwig <christiane.hertwig@googlemail.com>
2022-08-16 11:42:46 +0200

    bla

--- src/components/StyledShoppingItem.js
+++ src/components/StyledShoppingItem.js
@@ -2,11 +2,17 @@ import styled from "styled-components";
 
 const StyledShoppingItem = styled.li`
   background-color: #c0edff;
-  width: 150px;
+  border: 2px solid #c0edff;
+  width: 100px;
   border-radius: 30px;
   margin: 10px;
   padding: 10px;
   list-style: none;
+  display: flex;
+  justify-content: center;
+  &:hover {
+    background-color: transparent;
+  }
 `;
 
 export default StyledShoppingItem;
--- src/components/StyledSuggestionItem.js
+++ src/components/StyledSuggestionItem.js
@@ -2,11 +2,17 @@ import styled from "styled-components";
 
 const StyledSuggestionItem = styled.li`
   background-color: #ecf5ad;
-  width: 150px;
+  border: 2px solid #ecf5ad;
+  width: 100px;
   border-radius: 30px;
   margin: 10px;
   padding: 10px;
   list-style: none;
+  display: flex;
+  justify-content: center;
+  &:hover {
+    background-color: transparent;
+  }
 `;
 
 export default StyledSuggestionItem;
